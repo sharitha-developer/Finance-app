73366e67caf84139970b2fbf25c6e0fbda8200f0
sharitha-developer <sharitha.developer@gmail.com>
2025-09-10 15:18:15 +1000

    adding New layout pages

--- app/dashboard/Transactions/new/page.tsx
+++ app/dashboard/Transactions/new/page.tsx
@@ -1,5 +1,36 @@
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import Link from "next/link";
+import { Card, CardHeader } from "@/components/ui/card";
+
 export default function NewTransactionPage() {
     return (
-        <div>new transaction</div>
+        
+        <div className="max-w-screen-xl mx-auto py-10">
+            <Link href="/">Home</Link>
+            <Breadcrumb>
+                <BreadcrumbList>
+                    <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                          <Link href="/dashboard">Home</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                     <BreadcrumbItem>
+                        <BreadcrumbLink asChild>
+                          <Link href="/dashboard/transactions">Transactions</Link>
+                        </BreadcrumbLink>
+                    </BreadcrumbItem>
+                    <BreadcrumbSeparator />
+                     <BreadcrumbItem>
+                        <BreadcrumbPage>New Transaction</BreadcrumbPage>
+                    </BreadcrumbItem>                    
+                </BreadcrumbList>
+            </Breadcrumb>
+            <Card className="mt-4 max-w-screen-md">
+               <CardHeader>
+                
+               </CardHeader>
+            </Card>
+        </div>
     );
 }
